175b3af7d00d6b6a636bee69b593e14f3425ab7b
Ashil John <willsquad.aj@gmail.com>
2018-01-17 16:00:51 +0530

    January 17 Commit

--- included/save_contact.php
+++ included/save_contact.php
@@ -299,62 +299,6 @@ if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ){
         
         echo "Contact(s) Deleted";
     }
-    else if(isset($_POST['to_export']) && isset($_POST['count'])) {
-        
-        $to_export_array = json_decode($_POST['to_export']);
-        $item_count = (int)$_POST['count'];
-
-        if($item_count > 0) {
-            
-                //echo $to_export_array[$i];
-
-                $result = $dbc->query("SELECT `c_id`, `c_fname`, `c_lname`, `c_mname`, `c_email`, `c_phone`, `c_organization`, `c_website`, `c_linkedin`, `c_twitter`, `c_facebook` FROM `contacts_8521` WHERE `added_by_u_id` = $user_id AND `is_deleted_contact` = 0");
-
-
-                if($result->num_rows > 0) { // Results found
-                    $rows = $result->fetch_all();
-                } else {
-                    exit();
-                }
-
-
-                //Get the column names.
-                $columnNames = array();
-                if(!empty($rows)){
-                    //We only need to loop through the first row of our result
-                    //in order to collate the column names.
-                    $firstRow = $rows[0];
-                    foreach($firstRow as $colName => $val){
-                        $columnNames[] = $colName;
-                    }
-                }
-                
-                //Setup the filename that our CSV will have when it is downloaded.
-                $fileName = 'contacts.csv';
-                
-                //Set the Content-Type and Content-Disposition headers to force the download.
-                header('Content-Type: application/excel');
-                header('Content-Disposition: attachment; filename="' . $fileName . '"');
-                
-                //Open up a file pointer
-                $fp = fopen('php://output', 'w');
-                
-                //Start off by writing the column names to the file.
-                fputcsv($fp, $columnNames);
-                
-                //Then, loop through the rows and write them to the CSV file.
-                foreach ($rows as $row) {
-                    fputcsv($fp, $row);
-                }
-                
-                //Close the file pointer.
-                fclose($fp);
-            
-        }
-        
-        
-        echo "Contact(s) Exported";
-    }
 
 }
 
