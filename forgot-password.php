8e50dbc9238c8fee06a96e01eb6cf45a246cfe63
Ashil John <willsquad.aj@gmail.com>
2018-01-18 16:02:50 +0530

    January 18 Commit update

--- forgot-password.php
+++ forgot-password.php
@@ -23,7 +23,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
-        <?php echo (isset($title)? $title : 'Contact Manager') ?>
+        <?php echo (isset($title)? $title : 'Contact Manager | Forgot Password') ?>
     </title>
     <link rel="stylesheet" href="_files/css/bootstrap.css">
     <link rel="stylesheet" href="_files/css/style.css">
